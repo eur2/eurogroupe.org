669e7d0f2bf4567b7c57a295b15ed40e271824b5
eur2 <gregory.dapra@gmail.com>
2022-12-26 17:36:58 +0100

    v1

--- gatsby-browser.js
+++
@@ -1,6 +0,0 @@
-/*exports.onClientEntry = async () => {
-	if (typeof window.IntersectionObserver === `undefined`) {
-		await import(`intersection-observer`);
-		console.log(`👍 IntersectionObserver is polyfilled`);
-	}
-};*/
--- src/pages/404.js
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-
-export default () => (
-  <div>
-    <p>404: Not Found</p>
-  </div>
-);
